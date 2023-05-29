a4834ef7202e02e7f37f5c2e8bc7610cb58b19d4
Daniel Ajenifuja <335984019@educ.dpcdsb.org>
2023-05-29 09:30:30 -0400

    yes

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace password_checker
             Console.WriteLine("enter a username");
             string username = Console.ReadLine().ToLower();
             Console.WriteLine("enter a pass");
-            string passwords = Console.ReadLine().ToLower() ;
+            string passwords = Console.ReadLine().ToLower();
             //read data and splits
             using (StreamReader sr = new StreamReader(filename))
             {
@@ -30,14 +30,16 @@ namespace password_checker
                 }
 
             }
+
             //interation sort?
             string[] userPasswordsArray = pass.ToArray();
             for (int i = 1; i < userPasswordsArray.Length; i++)
             {
+
                 string current = userPasswordsArray[i];
                 int j = i - 1;
 
-                while (j >= 0 && userPasswordsArray[j].CompareTo(current) > 0)
+                while (j > 0 && userPasswordsArray[j].CompareTo(current) > 0)
                 {
                     userPasswordsArray[j + 1] = userPasswordsArray[j];
                     j--;
@@ -45,54 +47,58 @@ namespace password_checker
                 userPasswordsArray[j + 1] = current;
             }
 
-            // targets
-                string target1 = username;  
-                string target2 = passwords;  
-                bool found = false;
+            /* targets
+            string target1 = username;
+            string target2 = passwords;
+            */
+            bool found = false;
             // find targets for left and right
-                int left = 0;
-                int right = userPasswordsArray.Length - 1;
-
+            int mid = 0;
+            int left = 0;
+            int right = userPasswordsArray.Length - 1;
+            string[] midvalue;
             while (left <= right)
             {
                 // less than right divide by 2 find middle and check if matches
-                int mid = (left + right) / 2;
-                string[] split = userPasswordsArray[mid].Split(":");
-                string usernameFromData = split[0];
-                string passwordFromData = split[1];
-                // compare with targets
-                int comparison = string.Compare(usernameFromData, target1);
-
-                if (comparison == 0)
+                mid = (left + right) / 2;
+                midvalue = userPasswordsArray[mid].Split(":");
+                string usernameFromData = midvalue[0];
+                string passwordFromData = midvalue[1];
+                if (midvalue[0] == username)
                 {
-                    // pass target
-                    comparison = string.Compare(passwordFromData, target2);
-                         if (comparison == 0)
-                         {
+                    found = true;
+                    if (midvalue[1] == passwords)
+                    {
                         Console.WriteLine("match found!");
                         found = true;
                         break;
-                         }
-                }
-            // less than iteraite and move to next
-                    if (comparison < 0)
-                    {
-                        left = mid + 1;
+
                     }
-                    // dame
-                    else
-                    {
-                        right = mid - 1;
-                    }
-                }
-            //invalid
-                if (!found)
-                {
-                    Console.WriteLine("Invalid username or password.");
+                    else Console.WriteLine("incorrect password :(");
+                    break;
                 }
 
+                int comparison = string.Compare(usernameFromData, username + String.Compare(passwordFromData, passwords));
+
+       
+                // less than iteraite and move to next
+                if (comparison < 0)
+                {
+                    left = mid + 1;
+                }
+                // 
+                else
+                {
+                    right = mid - 1;
+                }
             }
+            //invalid
+            if (!found)
+            {
+                Console.WriteLine("Invalid username or password.");
+            }
+
         }
-         
     }
 
+}
