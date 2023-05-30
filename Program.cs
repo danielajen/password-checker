d943a02b4e7cdae92a1cf6e649eb0f9d7e970068
Daniel Ajenifuja <70176434+danielajen@users.noreply.github.com>
2023-05-29 23:18:32 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,34 +12,32 @@ namespace password_checker
             string filename = "userdatabase.txt";
             List<string> pass = new List<string>();
             // ask for user and pass
-            Console.WriteLine("enter a username");
+            Console.WriteLine("Enter a username:");
             string username = Console.ReadLine().ToLower();
-            Console.WriteLine("enter a pass");
-            string passwords = Console.ReadLine().ToLower();
-            //read data and splits
+            Console.WriteLine("Enter a password:");
+            string password = Console.ReadLine().ToLower();
+            // read data and split
             using (StreamReader sr = new StreamReader(filename))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
                     string[] split = line.Split(" ");
-                    string user = split[1];
-                    string password = split[2];
-                    string userPass = user + ":" + password;
+                    string user = split[0];
+                    string passwords = split[1];
+                    string userPass = user + ":" + passwords;
                     pass.Add(userPass);
                 }
-
             }
 
-            //interation sort?
+            // insertion sort
             string[] userPasswordsArray = pass.ToArray();
             for (int i = 1; i < userPasswordsArray.Length; i++)
             {
-
                 string current = userPasswordsArray[i];
                 int j = i - 1;
 
-                while (j > 0 && userPasswordsArray[j].CompareTo(current) > 0)
+                while (j >= 0 && userPasswordsArray[j].CompareTo(current) > 0)
                 {
                     userPasswordsArray[j + 1] = userPasswordsArray[j];
                     j--;
@@ -47,58 +45,37 @@ namespace password_checker
                 userPasswordsArray[j + 1] = current;
             }
 
-            /* targets
-            string target1 = username;
-            string target2 = passwords;
-            */
             bool found = false;
-            // find targets for left and right
-            int mid = 0;
+            // binary search
             int left = 0;
             int right = userPasswordsArray.Length - 1;
-            string[] midvalue;
             while (left <= right)
             {
-                // less than right divide by 2 find middle and check if matches
-                mid = (left + right) / 2;
-                midvalue = userPasswordsArray[mid].Split(":");
+                int mid = (left + right) / 2;
+                string[] midvalue = userPasswordsArray[mid].Split(":");
                 string usernameFromData = midvalue[0];
                 string passwordFromData = midvalue[1];
-                if (midvalue[0] == username)
+                if (usernameFromData == username && passwordFromData == password)
                 {
+                    Console.WriteLine("Match found!");
                     found = true;
-                    if (midvalue[1] == passwords)
-                    {
-                        Console.WriteLine("match found!");
-                        found = true;
-                        break;
-
-                    }
-                    else Console.WriteLine("incorrect password :(");
                     break;
                 }
-
-                int comparison = string.Compare(usernameFromData, username + String.Compare(passwordFromData, passwords));
-
-       
-                // less than iteraite and move to next
-                if (comparison < 0)
+                else if (string.Compare(usernameFromData, username) < 0)
                 {
                     left = mid + 1;
                 }
-                // 
                 else
                 {
                     right = mid - 1;
                 }
             }
-            //invalid
+
+            // invalid
             if (!found)
             {
                 Console.WriteLine("Invalid username or password.");
             }
-
         }
     }
-
 }
